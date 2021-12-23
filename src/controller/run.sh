d8ff0a43370cabc1ad5e4ec7856c7d126c0c0cfd
landaudiogo <diogo.hewitt.landau@hotmail.com>
2021-12-23 16:12:13 +0000

    Run script includes sym links for the controller

--- src/controller/run.sh
+++ src/controller/run.sh
@@ -2,6 +2,7 @@
 
 set -eo nounset
 
+
 IFS=$'\n' arrayRealFiles=(
   $(find . -type l -print | xargs -d '\n' -I @ realpath @)
 )
@@ -18,6 +19,7 @@ for file in "${arrayRealFiles[@]}"; do
   cp "$file" ./
 done
 
+
 replace_files_with_sym() {
   for file in "${arrayRealFiles[@]}"; do
     rm "$(basename $file)"
@@ -25,8 +27,9 @@ replace_files_with_sym() {
   done
 }
 
+trap "replace_files_with_sym" 0
+
 docker build -t controller-image . && 
-  { 
     docker run \
       -it --rm \
       --network kafka_kafka-network \
@@ -38,7 +41,4 @@ docker build -t controller-image . &&
       --add-host="prod1:172.31.32.34" \
       --add-host="prod2:172.31.30.37" \
       -v controller-volume:/usr/src/data \
-      controller-image || { replace_files_with_sym; exit 1; }
-  }
-
-replace_files_with_sym
+      controller-image
